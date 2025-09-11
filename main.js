15e4d849d0d5af9a3b65a46c225ce8153dcc0e52
AtaldaMd <144829296+AtaldaMd@users.noreply.github.com>
2025-09-11 08:58:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,175 +1,46 @@
-        // muda o nome a partir do seletor de classe
-        var pacientes = document.querySelector("pessoas");
-	    pacientes.textContent = "Meus Pacientes";
-		var aparecida = document.querySelector("nutrições");
-		aparecida.textContent = "Mariazinha nutrições";
-		var Nutrição = document.querySelector("h1");
-		Nutrição. textContent = "Apenas nutrições";
+// muda o nome a partir do seletor de classe
+var subtitulo = document.querySelector(".subtitulo");
+subtitulo.textContent = "Meus Pacientes";
+var nutricao = document.querySelector(".titulo");
+nutricao.textContent = "Mdzinha Nutrição";
 
-        // acessar a tag tr - paciente Paulo
-        var paciente = document.querySelector(".paciente");
-        for(var i = 0; i < paciente.length; i++){
-   //seleciona o coteúdo peso da tag
-   var tdPeso = paciente.querySelector(".info-peso");
-   var peso = tdPeso.textContent;
+//acessar a tag tr -menos o  paciente Paulo
+var pacientes = document.querySelectorAll(".paciente");
+for(var i =0; i < pacientes.length; i++){
+var paciente = pacientes[i];
 
-   var tdAltura = paciente.querySelector(".info-altura");
-   var altura = tdAltura.textContent;
 
-   var imc = peso / (altura * altura );
+    // Seleciona o conteúdo do peso da tag
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
 
-   //variáveis com valor true
-    var pesoValido = true;
-   var alturaValida = true;
+//Seleciona o conteúdo altura da tag
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
-   if (pesoValido && alturaValida){
-       // acessa e altera o imc
- var tdImc = paciente.querySelector(".info-peso");
- tdImc.textContent = imc;
- }
-   //define limites de peso e altura
-   if(peso < 0 || peso > 1000) {
-       aletrt("Peso inválido"); 
-       var pesoValido = false; 
-       tdImc.textContent = "Peso Inválido";
-   }
+//calcula o imc
+var imc = peso/ (altura * altura);
+imc = imc.toFixed(2);
 
-   if(altura < 0 || altura > 4.00){
-       alert("Altura inválida");
-       var alturaValida = false;
-       tdImc.textContent = "Altura Inválido";
-   }
+// variáveis com valor true
+var pesoValido = true;
+var alturaValida =  true;
 
-       // acessar a tag tr - paciente João
-   var paciente = document.querySelector("#segundo-paciente");
-   //seleciona o coteúdo peso da tag
-   var tdPeso = paciente.querySelector(".info-peso");
-   var peso = tdPeso.textContent;
+if(pesoValido && alturaValida){
+    // acessa e altera o imc
+    var tdImc = paciente.querySelector(".info-imc");
+    tdImc.textContent = imc;
+    }
 
-   var tdAltura = paciente.querySelector(".info-altura");
-   var altura = tdAltura.textContent;
-
-   var imc = peso / (altura * altura );
-  
-   //variáveis com valor true
-   var pesoValido = true;
-   var alturaValida = true;
-
-   if (pesoValido && alturaValida){
-       // acessa e altera o imc
- var tdImc = paciente.querySelector(".info-peso");
- tdImc.textContent = imc;
- }
-   //define limites de peso e altura
-   if(peso < 0 || peso > 1000) {
-       aletrt("Peso inválido"); 
-       var pesoValido = false; 
-       tdImc.textContent = "Peso Inválido";
-   }
-
-   if(altura < 0 || altura > 4.00){
-       alert("Altura inválida");
-       var alturaValida = false;
-       tdImc.textContent = "Altura Inválido";
-   } 
-
-   // acessar a tag tr - paciente Erica
-   var paciente = document.querySelector("#terceiro-paciente");
-   //seleciona o coteúdo peso da tag
-   var tdPeso = paciente.querySelector(".info-peso");
-   var peso = tdPeso.textContent;
-
-   var tdAltura = paciente.querySelector(".info-altura");
-   var altura = tdAltura.textContent;
-
-   var imc = peso / (altura * altura );
-
-   //variáveis com valor true
-   var pesoValido = true;
-   var alturaValida = true;
-
-   if (pesoValido && alturaValida){
-       // acessa e altera o imc
- var tdImc = paciente.querySelector(".info-peso");
- tdImc.textContent = imc;
- }
-   //define limites de peso e altura
-   if(peso < 0 || peso > 1000) {
-       aletrt("Peso inválido"); 
-       var pesoValido = false; 
-       tdImc.textContent = "Peso Inválido";
-   }
-
-   if(altura < 0 || altura > 4.00){
-       alert("Altura inválida");
-       var alturaValida = false;
-       tdImc.textContent = "Altura Inválido";
-   }
-   
-   // acessar a tag tr - paciente Douglas
-   var paciente = document.querySelector("#quarto-paciente");
-   //seleciona o coteúdo peso da tag
-   var tdPeso = paciente.querySelector(".info-peso");
-   var peso = tdPeso.textContent;
-
-   var tdAltura = paciente.querySelector(".info-altura");
-   var altura = tdAltura.textContent;
-
-   var imc = peso / (altura * altura );
-
-   //variáveis com valor true
-   var pesoValido = true;
-   var alturaValida = true;
-
-   if (pesoValido && alturaValida){
-       // acessa e altera o imc
- var tdImc = paciente.querySelector(".info-peso");
- tdImc.textContent = imc;
- }
-   //define limites de peso e altura
-   if(peso < 0 || peso > 1000) {
-       aletrt("Peso inválido"); 
-       var pesoValido = false; 
-       tdImc.textContent = "Peso Inválido";
-   }
-
-   if(altura < 0 || altura > 4.00){
-       alert("Altura inválida");
-       var alturaValida = false;
-       tdImc.textContent = "Altura Inválido";
-   }
-
-   // acessar a tag tr - paciente Tatiana
-   var paciente = document.querySelector("#quinto-paciente");
-   //seleciona o coteúdo peso da tag
-   var tdPeso = paciente.querySelector(".info-peso");
-   var peso = tdPeso.textContent;
-
-   var tdAltura = paciente.querySelector(".info-altura");
-   var altura = tdAltura.textContent;
-
-   var imc = peso / (altura * altura );
-
-   //variáveis com valor true
-   var pesoValido = true;
-   var alturaValida = true;
-
-   if (pesoValido && alturaValida){
-       // acessa e altera o imc
- var tdImc = paciente.querySelector(".info-peso");
- tdImc.textContent = imc;
- }
-   //define limites de peso e altura
-   if(peso < 0 || peso > 1000) {
-       aletrt("Peso inválido"); 
-       var pesoValido = false; 
-       tdImc.textContent = "Peso Inválido";
-   }
-
-   if(altura < 0 || altura > 4.00){
-       alert("Altura inválida");
-       var alturaValida = false;
-       tdImc.textContent = "Altura Inválido";
-   }
-
+    if(peso <= 0 || peso >= 1000){
+        var pesoValido = false;
+        tdImc.textContent = "peso inválido";
         }
+
+if(altura < 0 || altura > 3.00){
+    var alturaValida = false;
+    tdImc.textContent = "altura inválida";
+}
+
+
+}
