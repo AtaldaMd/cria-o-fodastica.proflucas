c5ae37ffc2e1194b9788ae11b09c3ae9679401f4
AtaldaMd <144829296+AtaldaMd@users.noreply.github.com>
2025-10-23 08:51:34 -0300

    Add files via upload

--- formulario.js
+++ formulario.js
@@ -11,12 +11,13 @@ var pacienteTr = criarTr(paciente);
 
 var tabela = document.querySelector("#tabela-pacientes");
 tabela.appendChild(pacienteTr);
+
+formulario.reset();
 });
 
 function BuscaValoresFormulario(formulario){
 //cria objeto paciente
     var paciente = {
-        
      nome: formulario.nome.value,
      peso: formulario.peso.value,
      altura: formulario.altura.value,
@@ -26,30 +27,26 @@ function BuscaValoresFormulario(formulario){
     return paciente;
 }
 
-function criarTr(){
+function criarTr(paciente){
     //é pra criar a tag tr da tabela 
     var pacienteTr = document.createElement("tr");
-
-    //é pra criar a tag td da tabela 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    //adiciona os valores
-    nomeTd.textContent = paciente.nome;
-    pesoTd.textContent = paciente.peso;
-    alturaTd.textContent = paciente.altura;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = paciente.imc;
+     pacienteTr.classList.add("paciente");
 
     //adiconar tags a tela do usuario 
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    pacienteTr.appendChild(criarTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(criarTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(criarTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(criarTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(criarTd(paciente.imc, "info-imc"));
 
     return pacienteTr;
+}
+
+function criarTd(dado,classe){
+   const td = document.createElement("td");
+
+   td.textContent = dado;
+   td.classList.add(classe);
+
+   return td;
 }
